338b20d4316c5abe15027daddf0767e72fe666d3
danderson100 <danderson7790@gmail.com>
2021-01-07 09:33:55 -0700

    updated layouts and formatting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.getElementById("container");
+const container = document.getElementById("sketch-container");
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
