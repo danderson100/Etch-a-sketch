ddcec94e4f44cf67b78d472a51c233747213c54d
danderson100 <danderson7790@gmail.com>
2021-01-08 08:47:29 -0700

    updated to it maintains the current color

--- script.js
+++ script.js
@@ -1,76 +1,117 @@
-/*
-* TODO: get slider data, update pixel functions
-*/
 
 const container = document.getElementById("sketch-container");
-
-function makeRows(rows, cols) {
-
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.addEventListener('mouseenter', () => cell.classList.add('black'));
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-//initial rows and columns;
-makeRows(16, 16);
 
 let slider = document.querySelector('.slider');
 slider.addEventListener('input', changePixels);
 
 
-function changePixels() {
-  let pixels = slider.value;
-
-  let gridPixels = container.querySelectorAll('div');
-  gridPixels.forEach(gridPixel => gridPixel.remove());
-
-  makeRows(pixels, pixels);
-  console.log(pixels);
-}
+let currColor = 'black';
 
 //stores all buttons
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => button.addEventListener('click', doButtonStuff));
 
+//function to generate the main etch-a-sketch grid
+function makeRows(rows, cols, color) {
+
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+
+  let gridPixels = container.querySelectorAll('div');
+
+  switch (color) {
+    case 'black':
+      setBlackPixels(gridPixels);
+      break;
+    case 'xmas':
+      setXmasPixels(gridPixels);
+      break;
+    case 'rainbow':
+      setRainbowPixels(gridPixels);
+      break;
+
+  }
+  
+};
+//initial rows and columns;
+makeRows(16, 16, 'black');
+
+/*
+* This function gets called by the slider, and it
+* adjusts the dimensions of the grid.
+*/
+function changePixels() {
+  let pixels = slider.value;
+
+  let gridPixels = container.querySelectorAll('div');
+  gridPixels.forEach(cell => cell.remove());
+
+  makeRows(pixels, pixels, currColor);
+}
+
 function doButtonStuff(e) {
   //TODO: remove this
   console.log(e.target.id);
 
   let btnSelection = e.target.id;
+
+  let gridPixels = container.querySelectorAll('div');
+
   switch (btnSelection) {
     case 'black':
-      console.log("black chosen...");
-      setBlackPixels();
+      currColor = 'black';
+      setBlackPixels(gridPixels);
       break;
     case 'xmas':
-      setXmasPixels();
+      currColor = 'xmas';
+      setXmasPixels(gridPixels);
       break;
     case 'rainbow':
-      setRainbowPixels();
+      currColor = 'rainbow';
+      setRainbowPixels(gridPixels);
       break;
     case 'reset':
-      resetPixels();
+      resetPixels(gridPixels);
       break;
   }
 
 }
 
-function setBlackPixels() {
+function setBlackPixels(gridPixels) {
   console.log("Setting pixels to black...");
+  gridPixels.forEach(cell => cell.addEventListener('mouseenter', () => cell.classList.add('black')));
 }
 
-function setXmasPixels() {
+function setXmasPixels(gridPixels) {
   console.log("Setting pixels to red & green...")
+
+  gridPixels.forEach(cell => cell.addEventListener('mouseenter', () => cell.classList.remove()));
+
+  gridPixels.forEach(redOrGreen);
+  
 }
 
-function setRainbowPixels() {
+function redOrGreen(cell) {
+  num = Math.floor((Math.random()) * 2);
+
+  if (num == 1) {
+    cell.addEventListener('mouseenter', () => cell.classList.add('xmas-green'));
+  } else {
+    cell.addEventListener('mouseenter', () => cell.classList.add('xmas-red'));
+  }
+}
+
+function setRainbowPixels(gridPixels) {
   console.log("Setting pixels to rainbow...");
 }
 
-function resetPixels() {
+function resetPixels(gridPixels) {
   console.log("Resetting...");
+  gridPixels.forEach(cell => cell.remove());
+  makeRows(16, 16, 'black');
 }
